dad65b38dbef49491241a38e8d146e688859001b
Syed Ashar Gillani <s84116683@china.huawei.com>
2021-04-03 12:54:26 +0500

    Added User Module, Model and Basic Service

---
+++ src/users/models/user.model.ts
@@ -0,0 +1,13 @@
+export class User {
+  constructor() {
+    this.id = 0;
+    this.name = '';
+    this.username = '';
+    this.email = '';
+  }
+
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+}
